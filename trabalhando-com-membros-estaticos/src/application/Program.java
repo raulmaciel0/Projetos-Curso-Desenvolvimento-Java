b9c6b3b5867c876a1f81919f7d6a01d979327b99
raulmaciel0 <raul.maciel015@gmail.com>
2023-10-23 10:16:00 -0300

    Testando como metodos estaticos não necessitam ser instanciados para utiliza-los Ao contrario de metodos que pertencem a classes, os quais necessitam ser instaciados para o uso

---
+++ trabalhando-com-membros-estaticos/src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import util.Calculator;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program { // Testando como metodos estaticos não necessitam ser instanciados para utiliza-los
+                       // Ao contrario de metodos que pertencem a classes, os quais necessitam ser instaciados para o uso
+//  public static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Calculator calculator = new Calculator();
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double c = calculator.circumference(radius);
+        double v = calculator.volume(radius);
+
+        System.out.printf("Cincumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.println("Pi value: " + calculator.PI);
+        sc.close();
+    }
+//    public static double circumference(double radius){
+//        return 2 * PI * radius;
+//    }
+//
+//    public static double volume(double radius){
+//        return 4 * PI * radius * radius * radius / 3;
+//    }
+
+
+}
